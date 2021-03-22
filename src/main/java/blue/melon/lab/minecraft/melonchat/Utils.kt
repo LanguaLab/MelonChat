e284f603c20a224688fd2a350b599da3e9b6e283
lori3f6 <lori3f6@gmail.com>
2021-03-22 21:07:34 +0800

    (clean up)comment out unused codes

--- src/main/java/blue/melon/lab/minecraft/melonchat/Utils.kt
+++ src/main/java/blue/melon/lab/minecraft/melonchat/Utils.kt
@@ -1,38 +1,13 @@
 package blue.melon.lab.minecraft.melonchat
 
 import blue.melon.lab.minecraft.melonchat.message.Message
-import blue.melon.lab.minecraft.melonchat.message.Text
-import blue.melon.lab.minecraft.melonchat.message.TextFormat
-import com.google.gson.Gson
 import net.md_5.bungee.api.ChatColor
-import kotlin.random.Random
 
 object Utils {
-    private val colorFormatterRegex = "[&][0-fk-or]|[&][#][0-f]{6}".toRegex()
+    //private val colorFormatterRegex = "[&][0-fk-or]|[&][#][0-f]{6}".toRegex()
+    //private val gsonInstance = Gson()
     private val hexColorPattern: Regex = "[&][#][0-f]{6}".toRegex()
-    private val gsonInstance = Gson()
-    private val chars = "23456789ABCDEFHKMNPQRSTUVWXYZ+$%"
-
-    fun serialize(text: String): String {
-        val textList = ArrayList<Text>()
-        val format = TextFormat()
-        val elements = text.split(colorFormatterRegex)
-        val formatCodes = colorFormatterRegex.findAll(text)
-        val formatCodeIterator = formatCodes.iterator()
-
-        textList.add(Text("", format))
-
-        elements.forEach {
-            if (it.isNotEmpty()) {
-                textList.add(Text(it, format))
-            }
-            if (formatCodeIterator.hasNext()) {
-                format.alternate(formatCodeIterator.next().value)
-            }
-        }
-
-        return gsonInstance.toJson(textList)
-    }
+    private const val chars = "23456789ABCDEFHKMNPQRSTUVWXYZ+$%"
 
     fun applyFormatCode(message: String): String {
         return ChatColor.translateAlternateColorCodes('&', convertConvenientHexColorCode(message))
@@ -67,41 +42,63 @@ object Utils {
         return codeBuilder.toString()
     }
 
-    fun getBungeeFormatCode(format: String): ChatColor {
-        when (format) {
-            "&0" -> return ChatColor.BLACK
-            "&1" -> return ChatColor.DARK_BLUE
-            "&2" -> return ChatColor.DARK_GREEN
-            "&3" -> return ChatColor.DARK_AQUA
-            "&4" -> return ChatColor.DARK_RED
-            "&5" -> return ChatColor.DARK_PURPLE
-            "&6" -> return ChatColor.GOLD
-            "&7" -> return ChatColor.GRAY
-            "&8" -> return ChatColor.DARK_GRAY
-            "&9" -> return ChatColor.BLUE
-            "&a" -> return ChatColor.GREEN
-            "&b" -> return ChatColor.AQUA
-            "&c" -> return ChatColor.RED
-            "&d" -> return ChatColor.LIGHT_PURPLE
-            "&e" -> return ChatColor.YELLOW
-            "&f" -> return ChatColor.WHITE
+//    //unused
+//    fun serialize(text: String): String {
+//        val textList = ArrayList<Text>()
+//        val format = TextFormat()
+//        val elements = text.split(colorFormatterRegex)
+//        val formatCodes = colorFormatterRegex.findAll(text)
+//        val formatCodeIterator = formatCodes.iterator()
+//
+//        textList.add(Text("", format))
+//
+//        elements.forEach {
+//            if (it.isNotEmpty()) {
+//                textList.add(Text(it, format))
+//            }
+//            if (formatCodeIterator.hasNext()) {
+//                format.alternate(formatCodeIterator.next().value)
+//            }
+//        }
+//
+//        return gsonInstance.toJson(textList)
+//    }
 
-            "&k" -> return ChatColor.MAGIC
-            "&l" -> return ChatColor.BOLD
-            "&m" -> return ChatColor.STRIKETHROUGH
-            "&n" -> return ChatColor.UNDERLINE
-            "&o" -> return ChatColor.ITALIC
-            "&r" -> return ChatColor.RESET
-
-            else -> return ChatColor.of(format.substring(1, 8))
-        }
-    }
-
-    fun getRandomString(length: Int): String {
-        val stringBuilder = StringBuilder()
-        for (i in 1..length) {
-            stringBuilder.append(chars[Random.nextInt(0, chars.length)])
-        }
-        return stringBuilder.toString()
-    }
+//    fun getBungeeFormatCode(format: String): ChatColor {
+//        when (format) {
+//            "&0" -> return ChatColor.BLACK
+//            "&1" -> return ChatColor.DARK_BLUE
+//            "&2" -> return ChatColor.DARK_GREEN
+//            "&3" -> return ChatColor.DARK_AQUA
+//            "&4" -> return ChatColor.DARK_RED
+//            "&5" -> return ChatColor.DARK_PURPLE
+//            "&6" -> return ChatColor.GOLD
+//            "&7" -> return ChatColor.GRAY
+//            "&8" -> return ChatColor.DARK_GRAY
+//            "&9" -> return ChatColor.BLUE
+//            "&a" -> return ChatColor.GREEN
+//            "&b" -> return ChatColor.AQUA
+//            "&c" -> return ChatColor.RED
+//            "&d" -> return ChatColor.LIGHT_PURPLE
+//            "&e" -> return ChatColor.YELLOW
+//            "&f" -> return ChatColor.WHITE
+//
+//            "&k" -> return ChatColor.MAGIC
+//            "&l" -> return ChatColor.BOLD
+//            "&m" -> return ChatColor.STRIKETHROUGH
+//            "&n" -> return ChatColor.UNDERLINE
+//            "&o" -> return ChatColor.ITALIC
+//            "&r" -> return ChatColor.RESET
+//
+//            else -> return ChatColor.of(format.substring(1, 8))
+//        }
+//    }
+//
+//    fun getRandomString(length: Int): String {
+//        val stringBuilder = StringBuilder()
+//        for (i in 1..length) {
+//            stringBuilder.append(chars[Random.nextInt(0, chars.length)])
+//        }
+//        return stringBuilder.toString()
+//    }
 }
